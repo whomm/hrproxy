09df38683894837094b0acc9ad240db28ed711b4
whomm <weimingwei@vipkid.com.cn>
2018-12-17 11:01:35 +0800

    add

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/whomm/hrproxy/httptool"
 )
 
 func main() {
-	fmt.Println("hello")
 	serv := httptool.NewServer(":8091", "http://127.0.0.1:9200")
 	serv.Handler(`^.*/_cat/?.*$`, 2, nil)
 	serv.ListenAndServe()
